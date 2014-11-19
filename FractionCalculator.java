f5977a3aeeb1c12c5098d59c367585c37bfd0628
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-19 14:23:45 +0000

    basic shell added

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,4 +1,16 @@
 public class FractionCalculator{
-	private
 
+	private Fraction fraction;
+	private String inputString;
+
+		public FractionCalculator(){
+			this.fraction = null;
+			this.inputString = null;
+		}
+
+
+
+	public Fraction evaluate(Fraction fraction, String inputString){
+
+		}
 }

c18938361d2679160acee3eee70bea3bf2fc667a
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-20 23:01:17 +0000

    split string into numerator and denominator methods added, not tested yet

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,25 +1,57 @@
+import java.util.StringTokenizer;
+
 public class FractionCalculator{
 
 	private Fraction fraction;
 	private String inputString;
-	private char operand;
+	private String operand;
 
 		public FractionCalculator(){
 			this.fraction = null;
 			this.inputString = null;
-			this.operand = null;
+			this.operand = "";
+		}
+
+		//method to split string.
+		public Fraction splitString (String str){
+			Fraction newFrac = null;
+			StringTokenizer st = new StringTokenizer(str, " ");
+						while (st.hasMoreTokens()){
+						String strIn = st.nextToken();
+					}
+					return newFrac;
+		}
+		
+		//splits the string input into numerator and denominator.
+		public int newNumerator(String str){
+			char a;
+			int newNumer = 0;
+			for(int j = 0; j < str.length(); j++){
+				a = str.charAt(j);
+					if (a == '/'){
+						newNumer = Integer.parseInt(str.substring(0,j));
+					}
+				}
+			return newNumer;
+		}
+		//splits the string input into numerator and denominator.
+		public int newDenominator(String str){
+			char a;
+			int newDenom = 0;
+			for(int j = 0; j < str.length(); j++){
+				a = str.charAt(j);
+					if (a == '/'){
+						newDenom = Integer.parseInt(str.substring((j+1),str.length()));
+					}
+				}
+			return newDenom;
 		}
 
 
-		public Fraction(String str){
-
-		}
 
 	//Method to evaulate user fraction input.
 	public Fraction evaluate(Fraction fraction, String inputString){
-
-
-
-
+			Fraction newFrac = null;
+			return newFrac;
 		}
 }

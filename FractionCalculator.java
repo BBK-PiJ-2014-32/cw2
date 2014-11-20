e977597a68f572523d82951ba7bdc060c6abeefb
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-20 19:31:18 +0000

    Updating repo

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -2,15 +2,24 @@ public class FractionCalculator{
 
 	private Fraction fraction;
 	private String inputString;
+	private char operand;
 
 		public FractionCalculator(){
 			this.fraction = null;
 			this.inputString = null;
+			this.operand = null;
 		}
 
 
+		public Fraction(String str){
 
+		}
+
+	//Method to evaulate user fraction input.
 	public Fraction evaluate(Fraction fraction, String inputString){
+
+
+
 
 		}
 }

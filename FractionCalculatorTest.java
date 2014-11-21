bc31a63e31608b03116e607d4e67bfd859bb3671
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-21 11:30:52 +0000

    both numer and denom methods tested

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -2,10 +2,12 @@ public class FractionCalculatorTest{
 
 	 public static void main(String[] args) {
 
-
-		String str = "3/4 * 3/2";
+		String str = "3/4";
 		FractionCalculator FC1 = new FractionCalculator();
 		int test1 = FC1.newNumerator(str);
 		System.out.println(test1);
+		int test2 = FC1.newDenominator(str);
+		System.out.println(test2);
+
 	 }
 }

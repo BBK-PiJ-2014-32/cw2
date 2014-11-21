2ae96c2e3918d349024fa464987f52766484c92f
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-21 11:25:33 +0000

    newNumerator method tested

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -2,5 +2,10 @@ public class FractionCalculatorTest{
 
 	 public static void main(String[] args) {
 
+
+		String str = "3/4 * 3/2";
+		FractionCalculator FC1 = new FractionCalculator();
+		int test1 = FC1.newNumerator(str);
+		System.out.println(test1);
 	 }
 }

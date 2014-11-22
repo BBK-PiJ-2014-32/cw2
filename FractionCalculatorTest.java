c0dcb537d4e4c66c16acbdf7dc78daec6860b621
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-22 14:25:22 +0000

    splitString method tested

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -2,12 +2,15 @@ public class FractionCalculatorTest{
 
 	 public static void main(String[] args) {
 
-		String str = "3/4";
+		String str1 = "3/4";
 		FractionCalculator FC1 = new FractionCalculator();
-		int test1 = FC1.newNumerator(str);
+		int test1 = FC1.newNumerator(str1);
 		System.out.println(test1);
-		int test2 = FC1.newDenominator(str);
+		int test2 = FC1.newDenominator(str1);
 		System.out.println(test2);
-
+		Fraction test3 = FC1.splitString(str1);
+		String str2 = test3.toString();
+		System.out.println(str2);
+		
 	 }
 }
